0ea1807c2c1a479c683f9f0c95ecba4809959227
unparalleled-js <yingthi@live.com>
2020-06-10 20:51:02 -0500

    Add more

--- src/pdsite/Components/App.js
+++ src/pdsite/Components/App.js
@@ -9,6 +9,8 @@ import SelectedPillsTab from 'Images/selected_pills_tab.png'
 import NewPillButton from 'Images/new_pill_button.png'
 import DefaultPills from 'Images/default_pills.png'
 import SetPillProps from 'Images/set_pill_props.png'
+import SimpleSites from 'Images/simple_sites.png'
+import DoubleSites from 'Images/double_sites.png'
 
 const App = () => (
   <div>
@@ -67,7 +69,20 @@ const App = () => (
     <br/>
     <Card id="ConfigureSites">
       <div><h2 className='title'>Configure Site Settings</h2></div>
-      <p>Create sites to match the order you wish to have autofill work.</p>
+      <p>
+        Create sites to match the order you wish to have autofill work.
+        Edit site details by tapping one of the sites or inserting a new site
+        with the green plus button.
+      </p>
+      <p>Change the site name or image associated with it.</p>
+      <img src={SimpleSites} alt="Simple sites" />
+      <p>Another use case might be that you use the same site 2x in a row.
+      Here is how you would achieve that: Add more sites with the green plus
+      button and then use the name picker to select the names of the other sites.
+      Also choose the corresponding site image if you want the full effect.
+      Here is what that would look like:
+      </p>
+      <img src={DoubleSites} alt="Double sites" />
     </Card>
   </div>
 );
