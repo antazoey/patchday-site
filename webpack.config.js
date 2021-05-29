4838a955f425933ac2c88f84bea2ad0eead6c4f3
unparalleled-js <yingthi@live.com>
2021-05-29 09:11:24 -0500

    babel load simp

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
       },
